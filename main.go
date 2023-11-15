68394dfa0f42572874f0d907dd3a9e53f7a88ecb
Sun Hyuk Ahn <joycaleb9705@berkeley.edu>
2023-11-14 20:48:24 -0800

    feat: remove redirect

--- main.go
+++ main.go
@@ -11,6 +11,15 @@ import (
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 
+	fmt.Print("Action (select between 'create' and 'remove'): ")
+	action, _ := reader.ReadString('\n')
+	action = strings.TrimSpace(action)
+
+	if action != "create" && action != "remove" {
+		fmt.Println("Unsupported action :(")
+		return
+	}
+
 	fmt.Print("Bucket Name: ")
 	bucket, _ := reader.ReadString('\n')
 	bucket = strings.TrimSpace(bucket)
@@ -19,18 +28,32 @@ func main() {
 	object, _ := reader.ReadString('\n')
 	object = strings.TrimSpace(object)
 
-	fmt.Print("Redirect Path: ")
-	path, _ := reader.ReadString('\n')
-	path = strings.TrimSpace(path)
+	switch action {
+	case "create":
+		fmt.Print("Redirect Path: ")
+		path, _ := reader.ReadString('\n')
+		path = strings.TrimSpace(path)
 
-	// New line for readability
-	fmt.Println()
+		// New line for readability
+		fmt.Println()
 
-	err := redirect.CreateRedirect(bucket, object, path)
-	if err != nil {
-		fmt.Printf("Error: %s\n", err)
-		return
+		err := redirect.RunAction("create", bucket, object, path)
+		if err != nil {
+			fmt.Printf("Error: %s\n", err)
+			return
+		}
+		fmt.Printf("Redirect from s3://%s/%s to %s created!\n", bucket, object, path)
+
+	case "remove":
+		// New line for readability
+		fmt.Println()
+
+		err := redirect.RunAction("remove", bucket, object, "")
+		if err != nil {
+			fmt.Printf("Error: %s\n", err)
+			return
+		}
+		fmt.Printf("Redirect s3://%s/%s removed!\n", bucket, object)
 	}
 
-	fmt.Printf("Redirect from s3://%s/%s to %s created!\n", bucket, object, path)
 }
--- redirect/redirect.go
+++ redirect/redirect.go
@@ -7,7 +7,7 @@ import (
 	"os/exec"
 )
 
-func CreateRedirect(bucket, object, path string) error {
+func RunAction(action, bucket, object, path string) error {
 	file, err := os.OpenFile("app.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		log.Fatal(err)
@@ -15,7 +15,14 @@ func CreateRedirect(bucket, object, path string) error {
 	defer file.Close()
 	log.SetOutput(file)
 
-	output, err := uploadObject(bucket, object, path)
+	var output []byte
+	switch action {
+	case "create":
+		output, err = createRedirect(bucket, object, path)
+	case "remove":
+		output, err = removeRedirect(bucket, object)
+	}
+
 	if err != nil {
 		log.Printf(err.Error())
 		return err
@@ -26,7 +33,7 @@ func CreateRedirect(bucket, object, path string) error {
 	return nil
 }
 
-func uploadObject(bucket, object, path string) ([]byte, error) {
+func createRedirect(bucket, object, path string) ([]byte, error) {
 	// Create temp file named object
 	tempFile, err := os.Create(object)
 	if err != nil {
@@ -35,7 +42,7 @@ func uploadObject(bucket, object, path string) ([]byte, error) {
 	defer tempFile.Close()
 
 	// Call S3-cli command and write to log
-	bucketPath := fmt.Sprintf("s3://%s/%s", bucket, object)
+	bucketPath := getBucketPath(bucket, object)
 	cmd := exec.Command("aws", "s3", "cp", object, bucketPath, "--acl", "public-read", "--website-redirect", path)
 	output, err := cmd.Output()
 	if err != nil {
@@ -49,4 +56,18 @@ func uploadObject(bucket, object, path string) ([]byte, error) {
 	}
 
 	return output, nil
+}
+
+func removeRedirect(bucket, object string) ([]byte, error) {
+	bucketPath := getBucketPath(bucket, object)
+	cmd := exec.Command("aws", "s3", "rm", bucketPath)
+	output, err := cmd.Output()
+	if err != nil {
+		return nil, err
+	}
+
+	return output, nil
+}
+func getBucketPath(bucket, object string) string {
+	return fmt.Sprintf("s3://%s/%s", bucket, object)
 }
